d41f8a0f7e6181566f95d9e4edad9ab335c36c5e
Bart <none@none>
2012-11-25 00:36:58 +0100

    Created thread for AudioInput processing loop. Added menu to toggle audio input.

--- src/gui/mainwindow.cpp
+++ src/gui/mainwindow.cpp
@@ -4,13 +4,15 @@
 #include "src/basic/animation.h"
 #include "src/player/player.h"
 #include "src/studio/studio.h"
+#include "src/audioinput/audioinput.h"
 
 
 #include <QTime>
 
-
+#include <thread>
 #include <memory>
 #include <unistd.h> // for usleep on linux
+
 
 MainWindow::MainWindow(QWidget *parent) :
   QMainWindow(parent),
@@ -18,11 +20,17 @@ MainWindow::MainWindow(QWidget *parent) :
   m_colorDialog(),
   m_nLedsTotal(160),
   m_player(new Player()),
-  m_studio(new Studio(m_nLedsTotal))
+  m_studio(new Studio(m_nLedsTotal)),
+  m_audioInput(new AudioInput()),
+  m_isAudioOn(false)
 {
   setWindowTitle("LED Controller");
 
   ui->setupUi(this);
+
+  createActions();
+  createToolbar();
+  createMenus();
 
   connectAllSlots();
 
@@ -31,12 +39,12 @@ MainWindow::MainWindow(QWidget *parent) :
   //  Animation animation;
 
 
-  for (std::size_t i = 0; i < 1000; ++i)
-  {
-    Color color(cos(i/M_PI)*177, sin(i/M_PI)*177, cos(2*i/M_PI)*177);
-    Animation animation = m_studio->createMovingLine(color);
-    m_player->play(animation);
-  }
+//  for (std::size_t i = 0; i < 1000; ++i)
+//  {
+//    Color color(cos(i/M_PI)*177, sin(i/M_PI)*177, cos(2*i/M_PI)*177);
+//    Animation animation = m_studio->createMovingLine(color);
+//    m_player->play(animation);
+//  }
 
 }
 
@@ -47,6 +55,61 @@ MainWindow::~MainWindow()
   delete m_studio;
 }
 
+
+void
+MainWindow::createActions()
+{
+  newAct = new QAction(tr("&New"), this);
+  newAct->setShortcuts(QKeySequence::New);
+  newAct->setStatusTip(tr("Start Audio Input Control Panel"));
+  connect(newAct, SIGNAL(triggered()), this, SLOT(slotToggleAudioInput()));
+}
+
+void
+MainWindow::createToolbar()
+{
+  fileToolBar = addToolBar(tr("File"));
+  fileToolBar->addAction(newAct);
+}
+
+void
+MainWindow::createMenus()
+{
+  fileMenu = menuBar()->addMenu(tr("&File"));
+  fileMenu->addAction(newAct);
+
+  editMenu = menuBar()->addMenu(tr("&Edit"));
+
+  helpMenu = menuBar()->addMenu(tr("&Help"));
+}
+
+
+void
+MainWindow::startAudioInputThread()
+{
+  m_isAudioOn = true;
+  std::cout << "MainWindow::startAudioInputThread()" << std::endl;
+  std::thread t1(&MainWindow::startAudioInput, this);
+  t1.detach();
+}
+
+void
+MainWindow::startAudioInput()
+{
+  m_audioInput->openStream();
+  m_audioInput->setLedPlayer(m_player);
+  m_audioInput->startStream();
+}
+
+
+void
+MainWindow::stopAudioInput()
+{
+  std::cout << "MainWindow::stopAudioInput()" << std::endl;
+  m_audioInput->setLedPlayer(m_player);
+  m_audioInput->closeStream();
+  m_isAudioOn = false;
+}
 
 void
 MainWindow::connectAllSlots() const
@@ -63,7 +126,7 @@ MainWindow::slotColorSelected(const QColor &color)
 {
   Color colorNew(color.red()/255.0*127.0, color.green()/255.0*127.0, color.blue()/255.0*127.0);
 
-  Animation animation = m_studio->createSingleColorSingleFrameAnimation(colorNew, m_nLedsTotal);
+  Animation animation = m_studio->createSingleColorSingleFrameAnimation(colorNew);
 
   m_player->play(animation);
 }
@@ -77,7 +140,21 @@ MainWindow::slotBrightnessChanged()
   int blue = ui->brightnessBlueHorizontalSlider->value();
   Color color(red, green, blue);
 
-  Animation animation = m_studio->createSingleColorSingleFrameAnimation(color, m_nLedsTotal);
+  Animation animation = m_studio->createSingleColorSingleFrameAnimation(color);
 
   m_player->play(animation);
+}
+
+
+void
+MainWindow::slotToggleAudioInput()
+{
+  if (m_isAudioOn)
+  {
+    stopAudioInput();
+  }
+  else
+  {
+    startAudioInputThread();
+  }
 }
--- src/gui/mainwindow.h
+++ src/gui/mainwindow.h
@@ -4,6 +4,7 @@
 #include "src/basic/animation.h"
 
 #include <QMainWindow>
+#include <QToolBar>
 #include <QColorDialog>
 
 #include <iostream>
@@ -11,6 +12,7 @@
 
 class Studio;
 class Player;
+class AudioInput;
 
 namespace Ui {
 class MainWindow;
@@ -30,9 +32,16 @@ public:
 private slots:
   void slotColorSelected(const QColor &color);
   void slotBrightnessChanged();
+  void slotToggleAudioInput();
 
 private:
+  void createMenus();
+  void createActions();
+  void createToolbar();
+  void startAudioInput();
+  void stopAudioInput();
   void connectAllSlots() const;
+  void startAudioInputThread();
   
 private:
   Ui::MainWindow *ui;
@@ -40,6 +49,17 @@ private:
   std::size_t m_nLedsTotal;
   Player* m_player;
   Studio* m_studio;
+  AudioInput* m_audioInput;
+
+  QToolBar* fileToolBar;
+
+  QMenu* fileMenu;
+  QMenu* editMenu;
+  QMenu* helpMenu;
+
+  QAction* newAct;
+
+  bool m_isAudioOn;
 };
 
 #endif // MAINWINDOW_H
